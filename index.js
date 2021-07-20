dc2bb2c65fea8b512e02330ea5292bfb7ea58352
Tufahel Ahmed <60083437+Tufahel@users.noreply.github.com>
2021-07-20 16:55:32 +0600

    wokring

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/', (req, res) =>{
+    res.send("Its, Working!!");
+})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
